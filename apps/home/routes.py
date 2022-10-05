9e58e313106fc3a5a714cb9574c56d24357247f6
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-05 17:13:28 +0300

    Contact US - Added Route

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -3,11 +3,58 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from apps.home import blueprint
+import flask
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 
+from apps import mail
+from apps.home import blueprint
+from apps.config import Config
+
+from flask_mail import Message
+default_sender = Config.MAIL_DEFAULT_SENDER
+
+def send_email(to, subject, message):
+    
+    try:
+
+        msg = Message(
+            subject,
+            body=message,
+            recipients=[to],
+            sender=default_sender
+            )
+        
+        mail.send(msg)
+
+        return True, None
+    
+    except Exception as e:
+
+        print('Error sending email: ' + str( e))
+        return False, str( e )
+
+@blueprint.route('/contact/', methods=['GET', 'POST'])
+@login_required
+def contact():
+
+    msg = None
+
+    if flask.request.method == 'POST':
+
+        contact_name  = request.form.get('full_name')
+        contact_email = request.form.get('email')
+        contact_msg   = request.form.get('message') 
+
+        status, error = send_email(contact_email, 'Mail from: ' + contact_name, contact_msg)
+
+        if status:
+            msg = 'Message sent.'
+        else:
+            msg = 'Error: ' + error 
+
+    return render_template('contact/send-message.html', msg=msg)
 
 @blueprint.route('/index')
 @login_required
